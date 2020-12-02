4329e15a8788a0dcc105b8a8bdd341b4e593dc55
defektive <defektive@users.noreply.github.com>
2020-12-02 14:24:11 -0700

    sort -h | uniq > sort -u

--- bin/ar-get-cert-domains.sh
+++ bin/ar-get-cert-domains.sh
@@ -26,4 +26,4 @@ mkdir -p hosts/$HOST/recon
       | grep -oP "CN ?=[ ]*[^ ]+" \
       | cut -d= -f2 \
       | sed 's/^[ ]*//g'
-} | sort -u | tee hosts/$HOST/recon/ssl-cert-domains-${HOST}-${PORT}.txt
+} | sort -u | uniq | tee hosts/$HOST/recon/ssl-cert-domains-${HOST}-${PORT}.txt
