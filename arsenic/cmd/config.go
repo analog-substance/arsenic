eb74013f91b391a8025f15e474d01d1ceaf63b84
sm4rtshr1mp <sm4rtshr1mp@users.noreply.github.com>
2021-04-05 22:59:09 -0600

    More comments. Can remove/reset values from config. Can set keys that don't exist.

--- arsenic/cmd/config.go
+++ arsenic/cmd/config.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
+	"strings"
 
 	"github.com/defektive/arsenic/arsenic/lib/util"
 	// "github.com/pelletier/go-toml"
@@ -25,25 +27,44 @@ Helpful to see what scripts would be executed.`,
 		count := len(args)
 		switch count {
 		case 0:
-			printConfig()
-		case 1, 2: // 1 arg = get config value, 2 args = set config value
-			key := args[0]
-			if !viper.IsSet(key) {
-				fmt.Println("Key not found in config")
+			subKeysOnly, _ := cmd.Flags().GetBool("sub-keys")
+			if subKeysOnly {
+				printKeys(viper.AllSettings())
 				return
 			}
+			printConfig()
+		case 1, 2: // 1 arg = get/remove/reset config value, 2 args = set config value
+			key := args[0]
 
 			currentValue := viper.Get(key)
-			if count == 1 { // If only one argument, just display the current config value
+			if count == 1 { // If only one argument, display current value/keys or remove/reset key
+				if !viper.IsSet(key) {
+					fmt.Println("Key not found in config")
+					return
+				}
+
 				subKeysOnly, _ := cmd.Flags().GetBool("sub-keys")
 				if subKeysOnly {
-					if valueMap, ok := currentValue.(map[string]interface{}); ok {
-						for key := range valueMap {
-							fmt.Println(key)
-						}
-					} else {
-						fmt.Println("No sub-keys")
+					printKeys(currentValue)
+					return
+				}
+
+				removeOrReset, _ := cmd.Flags().GetBool("remove-reset")
+				if removeOrReset {
+					parentKey, childKey := splitIntoParentChild(key)
+					parentValue := viper.GetStringMap(parentKey)
+					if parentKey == "" {
+						parentValue = viper.AllSettings()
 					}
+
+					delete(parentValue, childKey)
+					viper.Set(parentKey, parentValue)
+					if parentKey == "" {
+						overwriteInMemConfig(parentValue)
+					} else {
+						overwriteInMemConfig(viper.AllSettings())
+					}
+					saveConfig()
 					return
 				}
 
@@ -56,27 +77,43 @@ Helpful to see what scripts would be executed.`,
 				return
 			}
 
-			newValue, err := convertToConfigType(currentValue, args[1])
+			newValue, err := matchConfigType(currentValue, args[1])
 			if err != nil {
 				fmt.Println(err)
 				return
 			}
 
-			fmt.Println("Writing Config")
-
-			file := viper.ConfigFileUsed()
-			if file == "" {
-				file = "arsenic.yaml"
+			// If parentKey is empty, we are setting a root key
+			// If parentValue is nil, we are setting a nested key which has not been set
+			parentKey, _ := splitIntoParentChild(key)
+			parentValue := viper.Get(parentKey)
+			if _, ok := parentValue.(map[string]interface{}); !ok &&
+				parentKey != "" && parentValue != nil {
+				fmt.Printf("Key '%s' is not a key/value object", parentKey)
+				return
 			}
-			createIfNotExist(file)
 
 			viper.Set(key, newValue)
-			viper.WriteConfig()
+			saveConfig()
 		}
 	},
 }
 
-func convertToConfigType(currentValue interface{}, userValue string) (interface{}, error) {
+func saveConfig() {
+	fmt.Println("Writing Config")
+
+	createConfigIfNotExist()
+
+	viper.WriteConfig()
+}
+
+func overwriteInMemConfig(configMap map[string]interface{}) error {
+	encodedConfig, _ := yaml.Marshal(configMap)
+	return viper.ReadConfig(bytes.NewReader(encodedConfig))
+}
+
+// Based on the current value of the config, attempts to return the user's value as the right type
+func matchConfigType(currentValue interface{}, userValue string) (interface{}, error) {
 	// Currently our config values are either bool, int, or string
 	if _, ok := currentValue.(bool); ok {
 		value, err := strconv.ParseBool(userValue)
@@ -92,11 +129,27 @@ func convertToConfigType(currentValue interface{}, userValue string) (interface{
 		return value, nil
 	} else if _, ok := currentValue.(string); ok {
 		return userValue, nil
+	} else if currentValue == nil { // If currentValue is nil, we are setting a new key and we must guess the value type
+		intValue, err := strconv.Atoi(userValue)
+		if err == nil {
+			return intValue, nil
+		}
+
+		boolValue, err := strconv.ParseBool(userValue)
+		if err == nil {
+			return boolValue, nil
+		}
+
+		return userValue, nil
 	}
 	return nil, fmt.Errorf("cannot set keys that are not of type int, string or bool")
 }
 
-func createIfNotExist(fileName string) {
+func createConfigIfNotExist() {
+	fileName := viper.ConfigFileUsed()
+	if fileName == "" {
+		fileName = "arsenic.yaml"
+	}
 	_, err := os.Stat(fileName)
 	if os.IsNotExist(err) {
 		file, err := os.Create(fileName)
@@ -129,7 +182,28 @@ func printConfig() {
 	}
 }
 
+func printKeys(value interface{}) {
+	if valueMap, ok := value.(map[string]interface{}); ok {
+		for key := range valueMap {
+			fmt.Println(key)
+		}
+	} else {
+		fmt.Println("No sub-keys")
+	}
+}
+
+func splitIntoParentChild(key string) (string, string) {
+	split := strings.Split(key, ".")
+	keyCount := len(split)
+	if keyCount == 1 {
+		return "", key
+	}
+
+	return strings.Join(split[:keyCount-1], "."), split[keyCount-1]
+}
+
 func init() {
 	rootCmd.AddCommand(configCmd)
 	configCmd.Flags().BoolP("sub-keys", "k", false, "display only the sub-keys")
+	configCmd.Flags().BoolP("remove-reset", "r", false, "remove key from the config or reset to default")
 }
