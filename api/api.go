20be48bab9f53212bc04fd4c10f6d67ee8568a4a
NoF0rte <nof0rte@users.noreply.github.com>
2022-01-14 01:16:36 +0000

    Cleaned up the code

---
+++ api/api.go
@@ -0,0 +1,102 @@
+package api
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/analog-substance/arsenic/api/models"
+	"github.com/analog-substance/arsenic/lib/host"
+	"github.com/analog-substance/arsenic/lib/util"
+	"github.com/gorilla/mux"
+)
+
+type Api struct {
+	router *mux.Router
+}
+
+func NewApi() Api {
+	return Api{
+		router: mux.NewRouter().StrictSlash(true),
+	}
+}
+
+func (api Api) Serve(port int) error {
+	api.routes()
+
+	address := fmt.Sprintf("localhost:%d", port)
+
+	fmt.Printf("[+] Listening on %s\n", address)
+	return http.ListenAndServe(address, api.router)
+}
+
+func (api Api) routes() {
+	apiRouter := api.router.PathPrefix("/api").Subrouter()
+
+	hostRouter := apiRouter.PathPrefix("/host").Subrouter()
+	hostRouter.Methods(http.MethodPost, http.MethodOptions).
+		Path("/review").
+		HandlerFunc(api.reviewHost)
+
+	hostRouter.Use(mux.CORSMethodMiddleware(hostRouter))
+}
+
+func (api Api) reviewHost(rw http.ResponseWriter, r *http.Request) {
+	api.setCorsHeaders(rw)
+
+	if r.Method == http.MethodOptions {
+		return
+	}
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var reviewHost models.ReviewHost
+	err := json.Unmarshal(reqBody, &reviewHost)
+	if err != nil {
+		log.Printf("reviewHost: %v\n", err)
+		api.genericError(rw)
+		return
+	}
+
+	if reviewHost.Reviewer == "" {
+		reviewHost.Reviewer = "operator"
+	}
+
+	hosts := host.Get(reviewHost.Host)
+	if len(hosts) == 0 {
+		log.Printf("reviewHost: No hosts matched - %s\n", reviewHost.Host)
+		api.genericError(rw)
+		return
+	}
+
+	reviewer := util.GetReviewer(reviewHost.Reviewer)
+	for _, host := range hosts {
+		host.SetReviewedBy(reviewer)
+		host.SaveMetadata()
+	}
+
+	api.genericSuccess(rw)
+}
+
+func (api Api) setCorsHeaders(rw http.ResponseWriter) {
+	rw.Header().Set("Access-Control-Allow-Origin", "*")
+	rw.Header().Set("Access-Control-Allow-Headers", "*")
+}
+
+func (api Api) genericError(rw http.ResponseWriter) {
+	json.NewEncoder(rw).Encode(struct {
+		Msg string
+	}{
+		Msg: "An error occurred",
+	})
+}
+
+func (api Api) genericSuccess(rw http.ResponseWriter) {
+	json.NewEncoder(rw).Encode(struct {
+		Status string
+	}{
+		Status: "OK",
+	})
+}
--- cmd/serve.go
+++ cmd/serve.go
@@ -1,14 +1,9 @@
 package cmd
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
 
-	"github.com/analog-substance/arsenic/lib/host"
-	"github.com/gorilla/mux"
+	"github.com/analog-substance/arsenic/api"
 	"github.com/spf13/cobra"
 )
 
@@ -17,91 +12,13 @@ var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Starts the arsenic HTTP API",
 	Run: func(cmd *cobra.Command, args []string) {
-		router := mux.NewRouter().StrictSlash(true)
-		routes(router)
-
-		router.Use(mux.CORSMethodMiddleware(router))
-
 		port, _ := cmd.Flags().GetInt("port")
-		address := fmt.Sprintf("localhost:%d", port)
 
-		fmt.Printf("[+] Listening on %s\n", address)
-		http.ListenAndServe(address, router)
+		err := api.NewApi().Serve(port)
+		if err != nil {
+			fmt.Println(err)
+		}
 	},
-}
-
-func routes(router *mux.Router) {
-	apiRouter := router.PathPrefix("/api").Subrouter()
-
-	hostRouter := apiRouter.PathPrefix("/host").Subrouter()
-	hostRouter.Methods(http.MethodPost, http.MethodOptions).
-		Path("/review").
-		HandlerFunc(reviewHost)
-
-	hostRouter.Use(mux.CORSMethodMiddleware(hostRouter))
-}
-
-func genericError(rw http.ResponseWriter) {
-	json.NewEncoder(rw).Encode(struct {
-		Msg string
-	}{
-		Msg: "An error occurred",
-	})
-}
-
-func genericSuccess(rw http.ResponseWriter) {
-	json.NewEncoder(rw).Encode(struct {
-		Status string
-	}{
-		Status: "OK",
-	})
-}
-
-func setCorsHeaders(rw http.ResponseWriter) {
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
-	rw.Header().Set("Access-Control-Allow-Headers", "*")
-}
-
-type reviewHostRequest struct {
-	Host     string `json:"host"`
-	Reviewer string `json:"reviewer"`
-}
-
-func reviewHost(rw http.ResponseWriter, r *http.Request) {
-	setCorsHeaders(rw)
-
-	if r.Method == http.MethodOptions {
-		return
-	}
-
-	reqBody, _ := ioutil.ReadAll(r.Body)
-
-	var reviewRequest reviewHostRequest
-	err := json.Unmarshal(reqBody, &reviewRequest)
-	if err != nil {
-		log.Printf("reviewHost: %v\n", err)
-		genericError(rw)
-		return
-	}
-
-	if reviewRequest.Reviewer == "" {
-		reviewRequest.Reviewer = "operator"
-	}
-
-	hosts := host.Get(reviewRequest.Host)
-	if len(hosts) == 0 {
-		log.Printf("reviewHost: No hosts matched - %s\n", reviewRequest.Host)
-		genericError(rw)
-		return
-	}
-
-	reviewer := getReviewer(reviewRequest.Reviewer)
-	for _, host := range hosts {
-		host.SetReviewedBy(reviewer)
-		host.SaveMetadata()
-	}
-
-	genericSuccess(rw)
 }
 
 func init() {
