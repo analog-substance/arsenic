d44a28eb8dd13a3f8b6f35b88c42b88770d04675
NoF0rte <nof0rte@users.noreply.github.com>
2022-05-04 16:58:48 -0600

    Arsenic serve can now also run `hugo serve`

--- cmd/serve.go
+++ cmd/serve.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/analog-substance/arsenic/api"
 	"github.com/spf13/cobra"
@@ -13,6 +16,22 @@ var serveCmd = &cobra.Command{
 	Short: "Starts the arsenic HTTP API",
 	Run: func(cmd *cobra.Command, args []string) {
 		port, _ := cmd.Flags().GetInt("port")
+		hugoPath, _ := cmd.Flags().GetString("hugo")
+
+		if hugoPath != "" {
+			go func() {
+				cmd := exec.CommandContext(context.Background(), "hugo", "server")
+
+				cmd.Stderr = os.Stderr
+				cmd.Stdout = os.Stdout
+				cmd.Dir = hugoPath
+
+				err := cmd.Run()
+				if err != nil {
+					fmt.Println(err)
+				}
+			}()
+		}
 
 		err := api.Serve(port)
 		if err != nil {
@@ -25,4 +44,5 @@ func init() {
 	rootCmd.AddCommand(serveCmd)
 
 	serveCmd.Flags().IntP("port", "p", 7433, "The port to listen on")
+	serveCmd.Flags().StringP("hugo", "H", "", "The path to the hugo directory")
 }
