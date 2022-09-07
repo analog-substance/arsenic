b92592b755a6c543783a77603d8359912bcc90e3
NoF0rte <nof0rte@users.noreply.github.com>
2022-09-07 14:53:31 -0600

    Started implemented the rest of the git required functions

--- lib/script/git.go
+++ lib/script/git.go
@@ -1,6 +1,7 @@
 package script
 
 import (
+	"fmt"
 	"os"
 	"os/exec"
 
@@ -20,53 +21,146 @@ type GitModule struct {
 func (m *GitModule) ModuleMap() map[string]tengo.Object {
 	if m.moduleMap == nil {
 		m.moduleMap = map[string]tengo.Object{
-			"pull":   &tengo.UserFunction{Name: "pull", Value: m.pull},
-			"commit": &tengo.UserFunction{Name: "commit", Value: m.commit},
-			"lock":   &tengo.UserFunction{Name: "lock", Value: m.lock},
+			"pull":   &tengo.UserFunction{Name: "pull", Value: m.tengoPull},
+			"commit": &tengo.UserFunction{Name: "commit", Value: m.tengoCommit},
+			"lock":   &tengo.UserFunction{Name: "lock", Value: m.tengoLock},
 		}
 	}
 	return m.moduleMap
 }
 
-func (m *GitModule) pull(args ...tengo.Object) (tengo.Object, error) {
-	if m.isGit {
-		cmd := exec.Command("git", "pull", "--rebase")
+func (m *GitModule) tengoPull(args ...tengo.Object) (tengo.Object, error) {
+	if !m.isGit {
+		return nil, nil
+	}
 
-		err := cmd.Run()
-		if err != nil {
-			util.LogWarn("pull failed")
+	cmd := exec.Command("git", "pull", "--rebase")
+
+	err := cmd.Run()
+	if err != nil {
+		util.LogWarn("pull failed")
+	}
+	return nil, err
+}
+
+func (m *GitModule) tengoCommit(args ...tengo.Object) (tengo.Object, error) {
+	if !m.isGit {
+		return nil, nil
+	}
+
+	if len(args) > 2 && len(args) <= 3 {
+		return nil, tengo.ErrWrongNumArguments
+	}
+
+	path, ok := tengo.ToString(args[0])
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "path",
+			Expected: "string",
+			Found:    args[0].TypeName(),
 		}
 	}
-	return nil, nil
+
+	message, ok := tengo.ToString(args[1])
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "message",
+			Expected: "string",
+			Found:    args[1].TypeName(),
+		}
+	}
+
+	mode := ""
+	if len(args) == 3 {
+		mode, ok = tengo.ToString(args[2])
+		if !ok {
+			return nil, tengo.ErrInvalidArgumentType{
+				Name:     "mode",
+				Expected: "string",
+				Found:    args[2].TypeName(),
+			}
+		}
+	}
+
+	return nil, m.commit(path, message, mode)
 }
-func (m *GitModule) commit(args ...tengo.Object) (tengo.Object, error) {
-	return nil, nil
+
+func (m *GitModule) commit(path string, msg string, mode string) error {
+	if !m.isGit {
+		return nil
+	}
+
+	err := m.add(path)
+	if err != nil {
+		return err
+	}
+
+	err = exec.Command("git", "commit", "-m", msg).Run()
+	if err != nil {
+		fmt.Println("nothing happened")
+		return nil
+	}
+
+	err = m.push()
+	if err == nil {
+		return nil
+	}
+
+	util.LogWarn("First push failed", err)
+
+	err = exec.Command("git", "pull", "--rebase").Run()
+	if err != nil {
+		util.LogWarn("pull rebase failed", err)
+		if mode == "reset" {
+			util.LogWarn("reset to origin")
+
+			rebaseCmd := exec.Command("git", "rebase", "--abort")
+			rebaseCmd.Stdout = os.Stdout
+			rebaseCmd.Stderr = os.Stderr
+
+			rebaseCmd.Run()
+
+			resetCmd := exec.Command("git", "reset", "--hard", "origin/master")
+			resetCmd.Stdout = os.Stdout
+			resetCmd.Stderr = os.Stderr
+
+			resetCmd.Run()
+		}
+		os.Exit(2)
+	}
+
+	return m.push()
 }
-func (m *GitModule) lock(args ...tengo.Object) (tengo.Object, error) {
+
+func (m *GitModule) push() error {
+	return exec.Command("git", "push").Run()
+}
+
+func (m *GitModule) add(path string) error {
+	return exec.Command("git", "add", path).Run()
+}
+
+func (m *GitModule) tengoLock(args ...tengo.Object) (tengo.Object, error) {
+	if !m.isGit {
+		return nil, nil
+	}
+
 	if len(args) != 2 {
 		return nil, tengo.ErrWrongNumArguments
 	}
 
-	m.pull()
+	m.tengoPull()
 
 	lockFile, _ := tengo.ToString(args[0])
 	msg, _ := tengo.ToString(args[1])
 	if util.FileExists(lockFile) {
 		util.LogWarn("can't lock a file that exists")
-		os.Exit(1)
+		stopScript()
 	}
 
 	// TODO: Write random base64 string to lock file
 
-	gitCommit(lockFile, msg, "reset")
+	m.commit(lockFile, msg, "reset")
 
 	return nil, nil
-}
-
-func gitCommit(path string, msg string, mode string) error {
-	if !gitModule.isGit {
-		return nil
-	}
-
-	return nil
 }
