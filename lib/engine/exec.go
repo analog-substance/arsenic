87d73b1fe177598a1b3a9e0bb646f60b4f97ba7c
NoF0rte <nof0rte@users.noreply.github.com>
2022-09-15 16:52:18 +0000

    Added a simple cmd object to the exec module.

--- lib/engine/exec.go
+++ lib/engine/exec.go
@@ -13,6 +13,7 @@ import (
 func (s *Script) ExecModuleMap() map[string]tengo.Object {
 	return map[string]tengo.Object{
 		"run_with_sig_handler": &tengo.UserFunction{Name: "run_with_sig_handler", Value: s.tengoRunWithSigHandler},
+		"cmd":                  &tengo.UserFunction{Name: "cmd", Value: s.tengoCmd},
 	}
 }
 
@@ -52,6 +53,82 @@ func (s *Script) tengoRunWithSigHandler(args ...tengo.Object) (tengo.Object, err
 	return nil, nil
 }
 
+func (s *Script) tengoCmd(args ...tengo.Object) (tengo.Object, error) {
+	if len(args) == 0 {
+		return toError(tengo.ErrWrongNumArguments), nil
+	}
+
+	cmdName, ok := tengo.ToString(args[0])
+	if !ok {
+		return toError(tengo.ErrInvalidArgumentType{
+			Name:     "cmd name",
+			Expected: "string",
+			Found:    args[0].TypeName(),
+		}), nil
+	}
+
+	var cmdArgs []string
+	for _, arg := range args[1:] {
+		cmdArg, ok := tengo.ToString(arg)
+		if !ok {
+			return toError(tengo.ErrInvalidArgumentType{
+				Name:     "cmd arg",
+				Expected: "string",
+				Found:    arg.TypeName(),
+			}), nil
+		}
+
+		cmdArgs = append(cmdArgs, cmdArg)
+	}
+
+	cmd := exec.CommandContext(context.Background(), cmdName, cmdArgs...)
+
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	return &tengo.ImmutableMap{
+		Value: map[string]tengo.Object{
+			"set_file_stdin": &tengo.UserFunction{
+				Name: "set_file_stdin",
+				Value: func(args ...tengo.Object) (tengo.Object, error) {
+					if len(args) == 0 {
+						return toError(tengo.ErrWrongNumArguments), nil
+					}
+
+					file, ok := tengo.ToString(args[0])
+					if !ok {
+						return toError(tengo.ErrInvalidArgumentType{
+							Name:     "file",
+							Expected: "string",
+							Found:    args[0].TypeName(),
+						}), nil
+					}
+
+					f, err := os.Open(file)
+					if err != nil {
+						return toError(err), nil
+					}
+					cmd.Stdin = f
+
+					return nil, nil
+				},
+			},
+			"run": &tengo.UserFunction{
+				Name: "run",
+				Value: func(args ...tengo.Object) (tengo.Object, error) {
+					err := s.runCmdWithSigHandler(cmd)
+					if err != nil {
+						return toError(err), nil
+					}
+
+					return nil, nil
+				},
+			},
+		},
+	}, nil
+}
+
 func (s *Script) runWithSigHandler(name string, args ...string) error {
 	cmd := exec.CommandContext(context.Background(), name, args...)
 
@@ -59,6 +136,10 @@ func (s *Script) runWithSigHandler(name string, args ...string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 
+	return s.runCmdWithSigHandler(cmd)
+}
+
+func (s *Script) runCmdWithSigHandler(cmd *exec.Cmd) error {
 	sigs := make(chan os.Signal)
 	signal.Notify(sigs, os.Interrupt, syscall.SIGTERM)
 
