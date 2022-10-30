561198b3e26fbe254775e86ea96a5cff376116bc
NoF0rte <nof0rte@users.noreply.github.com>
2022-10-30 11:55:18 -0600

    Added file_exists and dir_exists functions to the filepath module

--- lib/engine/filepath.go
+++ lib/engine/filepath.go
@@ -10,13 +10,14 @@ import (
 
 func (m *Script) FilePathModuleMap() map[string]tengo.Object {
 	return map[string]tengo.Object{
-		"join":       &tengo.UserFunction{Name: "join", Value: m.join},
-		"exists":     &tengo.UserFunction{Name: "exists", Value: m.exists},
-		"base":       &tengo.UserFunction{Name: "base", Value: m.base},
-		"abs":        &tengo.UserFunction{Name: "abs", Value: m.abs},
-		"ext":        &tengo.UserFunction{Name: "ext", Value: m.ext},
-		"glob":       &tengo.UserFunction{Name: "glob", Value: m.glob},
-		"from_slash": &tengo.UserFunction{Name: "from_slash", Value: m.fromSlash},
+		"join":        &tengo.UserFunction{Name: "join", Value: m.join},
+		"file_exists": &tengo.UserFunction{Name: "file_exists", Value: m.fileExists},
+		"dir_exists":  &tengo.UserFunction{Name: "dir_exists", Value: m.dirExists},
+		"base":        &tengo.UserFunction{Name: "base", Value: m.base},
+		"abs":         &tengo.UserFunction{Name: "abs", Value: m.abs},
+		"ext":         &tengo.UserFunction{Name: "ext", Value: m.ext},
+		"glob":        &tengo.UserFunction{Name: "glob", Value: m.glob},
+		"from_slash":  &tengo.UserFunction{Name: "from_slash", Value: m.fromSlash},
 	}
 }
 
@@ -34,7 +35,7 @@ func (m *Script) join(args ...tengo.Object) (tengo.Object, error) {
 	return &tengo.String{Value: filepath.Join(paths...)}, nil
 }
 
-func (m *Script) exists(args ...tengo.Object) (tengo.Object, error) {
+func (m *Script) fileExists(args ...tengo.Object) (tengo.Object, error) {
 	if len(args) != 1 {
 		return nil, tengo.ErrWrongNumArguments
 	}
@@ -50,6 +51,28 @@ func (m *Script) exists(args ...tengo.Object) (tengo.Object, error) {
 
 	obj := tengo.TrueValue
 	if !util.FileExists(path) {
+		obj = tengo.FalseValue
+	}
+
+	return obj, nil
+}
+
+func (m *Script) dirExists(args ...tengo.Object) (tengo.Object, error) {
+	if len(args) != 1 {
+		return nil, tengo.ErrWrongNumArguments
+	}
+
+	path, ok := tengo.ToString(args[0])
+	if !ok {
+		return nil, tengo.ErrInvalidArgumentType{
+			Name:     "path",
+			Expected: "string",
+			Found:    args[0].TypeName(),
+		}
+	}
+
+	obj := tengo.TrueValue
+	if !util.DirExists(path) {
 		obj = tengo.FalseValue
 	}
 
