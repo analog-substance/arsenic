63137a7922e1b1a758be7fb695a478a26723735a
NoF0rte <nof0rte@users.noreply.github.com>
2023-01-26 13:01:40 -0700

    Added `has_any_port` function to the tengo host object

--- lib/engine/arsenic_host.go
+++ lib/engine/arsenic_host.go
@@ -44,6 +44,15 @@ func makeArsenicHost(h *host.Host) *tengo.ImmutableMap {
 					return value, nil
 				},
 			},
+			"has_any_port": &tengo.UserFunction{
+				Name: "has_any_port",
+				Value: func(args ...tengo.Object) (tengo.Object, error) {
+					if h.Metadata.HasAnyPort() {
+						return tengo.TrueValue, nil
+					}
+					return tengo.FalseValue, nil
+				},
+			},
 			"files": &tengo.UserFunction{
 				Name: "files",
 				Value: func(args ...tengo.Object) (tengo.Object, error) {
