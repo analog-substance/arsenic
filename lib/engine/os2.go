639d1fa7c1b883d12b6e048f08c5c0d05e212b28
NoF0rte <nof0rte@users.noreply.github.com>
2023-02-09 14:35:43 -0700

    Added mkdir_temp to the os2 tengo module

--- lib/engine/os2.go
+++ lib/engine/os2.go
@@ -12,6 +12,7 @@ func (s *Script) OS2ModuleMap() map[string]tengo.Object {
 	return map[string]tengo.Object{
 		"write_file":         &tengo.UserFunction{Name: "write_file", Value: s.writeFile},
 		"regex_replace_file": &tengo.UserFunction{Name: "regex_replace_file", Value: s.regexReplaceFile},
+		"mkdir_temp":         &tengo.UserFunction{Name: "mkdir_temp", Value: s.mkdirTemp},
 	}
 }
 
@@ -96,4 +97,37 @@ func (s *Script) regexReplaceFile(args ...tengo.Object) (tengo.Object, error) {
 	}
 
 	return nil, nil
+}
+
+func (s *Script) mkdirTemp(args ...tengo.Object) (tengo.Object, error) {
+	if len(args) != 2 {
+		return toError(tengo.ErrWrongNumArguments), nil
+	}
+
+	dir, ok := tengo.ToString(args[0])
+	if !ok {
+		return toError(tengo.ErrInvalidArgumentType{
+			Name:     "dir",
+			Expected: "string",
+			Found:    args[0].TypeName(),
+		}), nil
+	}
+
+	pattern, ok := tengo.ToString(args[1])
+	if !ok {
+		return toError(tengo.ErrInvalidArgumentType{
+			Name:     "pattern",
+			Expected: "string",
+			Found:    args[1].TypeName(),
+		}), nil
+	}
+
+	tempDir, err := os.MkdirTemp(dir, pattern)
+	if err != nil {
+		return toError(err), nil
+	}
+
+	return &tengo.String{
+		Value: tempDir,
+	}, nil
 }
