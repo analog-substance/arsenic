61deebc075577109ea1f1ea041dfe277edab503a
Bradley D <defektive@users.noreply.github.com>
2024-01-04 20:45:11 -0700

    Dev/version release (#90)
    * move cmd to internal
    
    * goreleaser
    
    * pass version info in
    
    * update test data paths

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/analog-substance/arsenic/cmd"
+import "github.com/analog-substance/arsenic/internal/app/cmd"
+
+var version = "v0.0.0"
+var commit = "replace"
 
 func main() {
+	cmd.SetVersionInfo(version, commit)
 	cmd.Execute()
 }
