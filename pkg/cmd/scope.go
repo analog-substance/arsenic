9df1bf54b5227190d78653bc66f009027f3303a6
defektive <defektive@users.noreply.github.com>
2024-10-14 20:51:24 -0600

    make scopious dev-scope

--- pkg/cmd/scope.go
+++ pkg/cmd/scope.go
@@ -1,10 +1,33 @@
 package cmd
 
 import (
-	scopiusCmd "github.com/analog-substance/scopious/pkg/cmd"
+	"fmt"
+
+	"github.com/analog-substance/arsenic/pkg/scope"
+	scopiousCmd "github.com/analog-substance/scopious/pkg/cmd"
+
+	"github.com/spf13/cobra"
 )
 
+// scopeCmd represents the scope command
+var scopeCmd = &cobra.Command{
+	Use:   "scope",
+	Short: "Print all scope",
+	Long:  `Print all scope`,
+	Run: func(cmd *cobra.Command, args []string) {
+		domains, _ := scope.GetScope("domains")
+		ips, _ := scope.GetScope("ips")
+
+		allScope := append(domains, ips...)
+		for _, scopeItem := range allScope {
+			fmt.Println(scopeItem)
+		}
+	},
+}
+
 func init() {
-	scopiusCmd.RootCmd.Use = "scope"
-	rootCmd.AddCommand(scopiusCmd.RootCmd)
+	rootCmd.AddCommand(scopeCmd)
+
+	scopiousCmd.RootCmd.Use = "dev-scope"
+	rootCmd.AddCommand(scopiousCmd.RootCmd)
 }
